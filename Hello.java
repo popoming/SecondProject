035fa92e4cbc7062fe2d99b280ee986569b7b73b
popoming <popoming89@gmail.com>
2016-07-07 15:20:24 +0900

    added output "bitkeeper"

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
 		System.out.println("what is Git?");
-		System.out.println("DVS");
+		System.out.println("2002, Bitkeeper");
 	}
 }
