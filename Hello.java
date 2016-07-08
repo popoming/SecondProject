4b3e12585c6858330c770977cd13348596f4b133
popoming <popoming89@gmail.com>
2016-07-08 16:48:27 +0900

    conflict resolved

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
 		System.out.println("what is Git?");
+		System.out.println("That's DVCS");
 		System.out.println("2002, Bitkeeper");
 	}
 }
